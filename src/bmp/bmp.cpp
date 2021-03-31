cc1c03a225d95ce2227b08f4989da34ef27d147f
Long LeiLei <sukhovii.anastasia@yahoo.com>
2021-03-31 14:05:18 +0800

    Innitial commit

--- src/bmp/bmp.cpp
+++ src/bmp/bmp.cpp
@@ -16,32 +16,22 @@ BMP::BMP(std::string fileName){
 void BMP::createPixelData(){
     int pixelCount = header.size - header.image_offset; 
     size_t i{header.image_offset}; 
-    std::cout << std::hex << static_cast<unsigned int>(buffer.get()[header.image_offset]) << "\n"; 
-    std::cout << std::hex << static_cast<unsigned int>(buffer.get()[header.image_offset+1]) << "\n"; 
-    std::cout << std::hex << static_cast<unsigned int>(buffer.get()[header.image_offset+2]) << "\n"; 
+    // std::cout << std::hex << static_cast<unsigned int>(buffer.get()[header.image_offset]) << "\n"; 
+    // std::cout << std::hex << static_cast<unsigned int>(buffer.get()[header.image_offset+1]) << "\n"; 
+    // std::cout << std::hex << static_cast<unsigned int>(buffer.get()[header.image_offset+2]) << "\n"; 
 
+    //data[3 * (i * width + j)]
 
-    for(int bl{0}; bl < pixelCount/4; bl++, i+=4){ 
-        //blue[bl] = buffer.get()[i]; 
-        //std::cout << static_cast<short>(buffer.get()[i]) << " "; 
-        buffer.get()[i] = static_cast<char>(0x8f);  
+    //char* colorData = &(buffer.get()[header.image_offset]); 
+
+    for(int i{0}; i < dibHeader.width; i++){ 
+        for(int j{0}; j < dibHeader.height; j+=3){
+            buffer.get()[header.image_offset + j] = 255; 
+            // std::cout << "i: " << i << "j: " << j << " colorData: " 
+            // << colorData[i * dibHeader.width + j] << std::endl;   
+        }
+        
     }
-
-    i = header.image_offset+1; 
-
-    for(int gr{0}; gr < pixelCount/4; gr++, i+=4 ){
-        //blue[bl] = buffer.get()[i]; 
-        buffer.get()[i] = static_cast<char>(0x8f); 
-    }
-
-    //i = header.image_offset+2; 
-
-    // for(int r{0}; r < pixelCount/3; r++, i+=3 ){
-    //     //blue[bl] = buffer.get()[i]; 
-    //     buffer.get()[i] = 0; 
-    // }
-
-    
     Writer::getInstance().writeToFile("../result.bmp", header.size, std::move(buffer)); 
 
     
@@ -51,7 +41,7 @@ void BMP::readHeader(){
     
     std::unique_ptr<char[]> h (new char[14]); 
 
-    //check std::copy 
+    
     for(int i{0}; i < 14; i++){
         h[i] = buffer.get()[i]; 
     }
@@ -60,12 +50,12 @@ void BMP::readHeader(){
     std::cout << header.name[0] << " " << header.name[1] << " " << header.size << " " 
     << header.image_offset << "\n";  
 
-    //check for 4204
+    
     if(header.name[0] != 'B' && header.name[1] != 'M'){
         std::cout << 'change file format'; 
     } 
 
-    //define width and height 
+    
     std::unique_ptr<char[]> detailed_h (new char[40]);  
 
     for(int i{0}, g{14}; i < 40; i++, g++ ){
