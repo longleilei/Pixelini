226a13023da5825b830e8a814a97525baec91cc4
Long LeiLei <sukhovii.anastasia@yahoo.com>
2021-04-11 12:44:21 +0800

    set up opengl

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,57 @@
 #include <iostream>
 #include "files/exception.h"
 #include "bmp/bmp.h"
+#include "GLFW/glfw3.h"
 
 int main(){
-    std::string name{"../assets/land.bmp"}; 
-    std::string copy{"../assets/forWrite.bmp"}; 
+    // std::string name{"../assets/land.bmp"}; 
+    // std::string copy{"../assets/forWrite.bmp"}; 
     
-    try{
+    // try{
         
 
-        BMP bmp{name}; 
+    //     BMP bmp{name}; 
         
        
         
-    } catch(Excptn& errorPath){
-        std::cout<< errorPath.what() << " " << errorPath.getPath()<<std::endl; 
+    // } catch(Excptn& errorPath){
+    //     std::cout<< errorPath.what() << " " << errorPath.getPath()<<std::endl; 
+    // }
+
+
+     GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
     }
 
-    
-    
-    
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+
+
     
 }
