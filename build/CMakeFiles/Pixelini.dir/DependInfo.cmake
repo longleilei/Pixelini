8453e049a28ab8149e918b08b2cd2841c9c9a823
Long LeiLei <sukhovii.anastasia@yahoo.com>
2021-04-11 19:31:18 +0800

    added filters and fixed buffer

---
+++ build/CMakeFiles/Pixelini.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/leilei/Desktop/Pixelini/src/GLAD/glad.cpp" "/Users/leilei/Desktop/Pixelini/build/CMakeFiles/Pixelini.dir/src/GLAD/glad.cpp.o"
+  "/Users/leilei/Desktop/Pixelini/src/bmp/bmp.cpp" "/Users/leilei/Desktop/Pixelini/build/CMakeFiles/Pixelini.dir/src/bmp/bmp.cpp.o"
+  "/Users/leilei/Desktop/Pixelini/src/files/exception.cpp" "/Users/leilei/Desktop/Pixelini/build/CMakeFiles/Pixelini.dir/src/files/exception.cpp.o"
+  "/Users/leilei/Desktop/Pixelini/src/files/loader.cpp" "/Users/leilei/Desktop/Pixelini/build/CMakeFiles/Pixelini.dir/src/files/loader.cpp.o"
+  "/Users/leilei/Desktop/Pixelini/src/files/writer.cpp" "/Users/leilei/Desktop/Pixelini/build/CMakeFiles/Pixelini.dir/src/files/writer.cpp.o"
+  "/Users/leilei/Desktop/Pixelini/src/filters/histogram.cpp" "/Users/leilei/Desktop/Pixelini/build/CMakeFiles/Pixelini.dir/src/filters/histogram.cpp.o"
+  "/Users/leilei/Desktop/Pixelini/src/image/image.cpp" "/Users/leilei/Desktop/Pixelini/build/CMakeFiles/Pixelini.dir/src/image/image.cpp.o"
+  "/Users/leilei/Desktop/Pixelini/src/main.cpp" "/Users/leilei/Desktop/Pixelini/build/CMakeFiles/Pixelini.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../headers"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.1.sdk/System/Library/Frameworks/OpenGL.framework"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
